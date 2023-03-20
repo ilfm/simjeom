178c2897228534b158dc5cad57030fb0140e0194
ilfm <dnehd1828@gmail.com>
2023-03-20 17:54:27 +0900

    프로젝트 생성

---
+++ src/main/java/com/simjeom/simjeom/SimjeomApplication.java
@@ -0,0 +1,13 @@
+package com.simjeom.simjeom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimjeomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimjeomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simjeom/simjeom/SimjeomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simjeom.simjeom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimjeomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
